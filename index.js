035d076454d2fcf6bd03314519d76aa500ea3a14
HAFIZH <73621103+HAFizh-15@users.noreply.github.com>
2021-04-25 17:31:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const {
     GroupSettingChange
 } = require('@adiwajshing/baileys')
 const { color, bgcolor } = require('./lib/color')
-const { help } = require('./src/help')
 const { wait, simih, getBuffer, h2k, generateMessageID, getGroupAdmins, getRandom, banner, start, info, success, close } = require('./lib/functions')
 const { fetchJson, fetchText } = require('./lib/fetcher')
 const { recognize } = require('./lib/ocr')
